dd938d34bec5a4ce5fe9530041eff69f2641779f
IaKrevedgo <68410799+IaKrevedgo@users.noreply.github.com>
2020-09-18 15:26:28 +0500

    Add files via upload

--- test.py
+++ test.py
@@ -2,38 +2,93 @@ import os
 import time
 import sys
 import getpass
-import hashlib
-import passfile as logpass #не забываем, что хотели это все шифрануть еще
 import configparser
+import passfile as logpass #тянем пароли из файла и не забываем, что хотели это все шифрануть еще
+import mes as mes #подключаем фалй с сообщениями
 
 
-#тянем из конфига
+#имена подключаемых файлов
+cfile = 'config.ini'
+lfile = 'my.log'
+mfile = 'mes.py'
+
+#работаем с дополнительными файлами
 config = configparser.ConfigParser()
-config.read('config.ini', encoding='utf-8-sig')
-maxerr = int(config.get('Setup','maxerr')) #Количество неудачных попыток до блокировки
-t2 = int(config.get('Setup','blocktime')) #время блокировки
 
-exit =0
+#глобальные переменные
+
+def Log (mtype,id):
+    logmesE = mes.logmesE   #алармы
+    logmesI = mes.logmesI   #информационные
+    datetime  = time.localtime()
+    time_str = time.strftime("[%m/%d/%Y, %H:%M:%S]", datetime)
+
+    if not os.path.exists(lfile):
+        with open(lfile, 'w') as file:
+            file.write('[MAIN LOG]')
+            file.close()
+    else:       
+        try:
+            emes = '\n[A]' + time_str + ' ' + str(logmesE[id])
+            imes = '\n[I]' + time_str + ' ' + str(logmesI[id])
+            wmes = '\n[W]' + time_str + ' ' + str(logmesI[id])
+            if mtype == 'e':
+                with open(lfile, 'a') as f:
+                    f.write(emes)
+            elif mtype == 'i':
+                with open(lfile, 'a') as f:
+                    f.write(imes)
+            elif mtype == 'w':
+                with open(lfile, 'a') as f:
+                    f.write(wmes)
+        except KeyError:
+            pass
+        
+
+def useConfig (path):
+    global maxerr 
+    global t2
+    if not os.path.exists(cfile): #если файла нет создаем новый
+        Log ('e','100') 
+        with open(path, "a") as f:
+            config.write(f)
+            Log ('0','100')
+        config.add_section('block')
+        config.set("block", "maxerr", "3")
+        config.set("block", "blocktime", "10")
+        
+    elif os.path.exists(cfile):
+        try:
+            #тянем из конфига
+            config.read(path, encoding='utf-8-sig')
+            maxerr = int(config.get('block','maxerr')) #Количество неудачных попыток до блокировки
+            t2 = int(config.get('block','blocktime')) #время блокировки
+        except KeyError:
+            pass
+
+useConfig (cfile)
+exit = 0
+Log ('i','1')
 while exit == 0:
 
     #init
-    login = 0
+    LogSys = 0
     err=0
     t1=0
 
-    while login == 0:
+    while LogSys == 0:
         if err < maxerr:
             os.system('cls')
             print ('<----Вход в систему---->','\n')
-            log = input ('Введите имя (прописные русские буквы)> ')
-            pas = getpass.getpass ('Введите пароль (пароль не отображается)> ')
+            login = input ('Введите имя (прописные русские буквы)> ')
+            password = getpass.getpass ('Введите пароль (пароль не отображается)> ')
             
             try:
-                if pas == logpass.logpas[log]:
+                if password == logpass.logpas[login]:
                     print()
                     print('Вход разрешен')
                     time.sleep(1)
-                    login = 1
+                    LogSys = 1
                 else:
                     print()
                     print('Неверный логин или пароль ')
@@ -59,14 +114,14 @@ while exit == 0:
                     err = 0
 
     selmenu = 0
-    while login == 1:
+    while LogSys == 1:
 
         os.system('cls')
         print('<----Меню программы---->','\n')
         print('1.Запустить приложение')
         print('2.Настройки приложения')
         print('3.Выход из системы')
-        print('4.Закрыть программу', sep='\n\n')
+        print('4.Закрыть программу', '\n\n')
         selmenu = input('Ввыберите пункт меню> ')
         try:
             if   selmenu == '1': #основная программа
@@ -77,7 +132,7 @@ while exit == 0:
                 os.system('cls')
                 print('Осуществлен выход из системы')
                 time.sleep(2)
-                login = 0
+                LogSys = 0
                 
             elif selmenu == '4': #закрыть программу
                 os.system('cls')
